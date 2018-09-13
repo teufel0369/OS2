ad0203d44020650c8ba591333f3b53f1adf2b2cb
Chris Thompson <cthompson@armsbusinesssolutions.com>
2018-09-13 09:57:27 -0500

    Initial commit. Added script to kill processes using runaway processes using shared memory. Updated master.c. Created shared.h with shared memory struct (this will make things easier, kind of like working with objects.)

--- christop.2/master.c
+++ christop.2/master.c
@@ -1,6 +1,185 @@
 #include <stdio.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/shm.h>
+#include <signal.h>
+#include "shared.h"
 
-int main() {
-    printf("Hello, World!\n");
+/*prototypes*/
+void printHelpMessage(char*);
+pid_t r_wait(int*);
+int whoAmI();
+
+/* GLOBALS */
+Process *pid;
+SharedMemClock *shm;
+int numChildren;
+int sharedMemId;
+int queueId;
+int indexCounter;
+int numTerminated = 0;
+
+int main(int argc, char* const argv[]) {
+    char myString [1000];
+    pid_t childPid = 0;
+    int opt = 0;
+    int i, numChildren, maxChildren = 0;
+    opterr = 0;
+
+    /*get the options from the argv array*/
+    while((opt = getopt(argc, argv, "n:s::h")) != -1) {
+        switch (opt) {
+            case 'n':
+                numChildren = atoi(optarg);
+                break;
+
+            case 's':
+                maxChildren = atoi(optarg);
+                break;
+
+            case 'h':
+                printHelpMessage(argv[0]);
+                break;
+
+
+            default:
+                if (optopt == 'n' && argc != 3) {
+                    snprintf(myString, sizeof myString, "\n%s: Error: Option -%c requires an integer argument.\n",
+                             argv[0], optopt);
+                    fprintf(stderr, "%s", myString);
+                    exit(EXIT_FAILURE);
+
+                } else if ((optopt == 'n' && numChildren < 0)) {
+                    snprintf(myString, sizeof myString,
+                             "\n%s: Error: The number of workers must be greater than 0.\n", argv[0]);
+                    fprintf(stderr, "%s", myString);
+                    exit(EXIT_FAILURE);
+                } else if (optopt == 's' && maxChildren < 0) {
+                    snprintf(myString, sizeof myString, "\n%s: Error: The maximum number of workers must be greater than 0.\n", argv[0]);
+                }
+
+                printHelpMessage(argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    /* create shared memory segment */
+    if ((sharedMemId = shmget(SHARED_MEM_KEY, sizeof(SharedMemClock), IPC_CREAT | 0600)) < 0) {
+        perror("[-]ERROR: Failed to create shared memory segment.");
+        exit(errno);
+    }
+
+    /* attach the shared memory */
+    shm = shmat(sharedMemId, NULL, 0);
+
+
     return 0;
+}
+
+/*************************************************!
+ * @function    printHelpMessage
+ * @abstract    Prints usage statement
+ **************************************************/
+void printHelpMessage(char* argv) {
+    char usageChildren [1000];
+    char usageHelp [1000];
+    char usageError [1000];
+    char titleMenu [1000];
+
+    snprintf(titleMenu, sizeof titleMenu, "\n################################ HELP MENU ################################");
+    snprintf(usageChildren, sizeof usageChildren, "\nUSAGE: %s -n <NUMBER> -s <NUMBER>\nDESCRIPTION: Executes worker processes with -n allowing a max number of workers with -s.\n", argv);
+    snprintf(usageHelp, sizeof usageHelp, "\nUSAGE: %s -h\nDESCRIPTION: Displays help options.\n", argv);
+
+    fprintf(stderr, "%s", titleMenu);
+    fprintf(stderr, "%s", usageChildren);
+    fprintf(stderr, "%s", usageHelp);
+    fprintf(stderr, "%s", usageError);
+    exit(EXIT_SUCCESS);
+}
+
+/*************************************************!
+ * @function    r_wait
+ * @abstract    waits but doesn't block
+ * @param       stat_loc    return status
+ * @citation    pg. 72 Unix Systems Programming
+ **************************************************/
+pid_t r_wait(int* stat_loc){
+    int retval;
+
+    while(((retval = wait(stat_loc)) == -1) && (errno == EINTR));
+    return retval;
+}
+
+/*************************************************!
+ * @function    whoAmI
+ * @abstract    used for testing
+ * @param       stat_loc    return status
+ **************************************************/
+int whoAmI(){
+    printf("Child %d  My Parent is %d\n\n", getpid(), getppid());
+    return 1;
+}
+
+/*************************************************!
+* @function    detachAndRemove
+* @abstract    detach and remove any shared memory
+* @param       pidIndex index of the pidIndex
+* @returns     actual index from the pid array
+* @cite        Unix Systems Programming Pg. 528
+**************************************************/
+int detachAndRemove(int shmid, void *shmaddr) {
+    int error = 0;
+
+    if (shmdt(shmaddr) == -1)
+        error = errno;
+    if ((shmctl(shmid, IPC_RMID, NULL) == -1) && !error)
+        error = errno;
+    if (!error)
+        return 0;
+    errno = error;
+    return -1;
+}
+
+/*******************************************************!
+* @function    signalHandler
+* @abstract    performs all necessary signal handling
+*
+* @param       pidIndex index of the pidIndex
+* @returns     actual index from the pid array
+*******************************************************/
+void signalHandlerMaster(int signo) {
+    int i, wait_status;
+
+    if (signo == SIGINT || signo == SIGALRM) {
+        if (signo == SIGINT)
+            printf("MASTER: SIGNAL: SIGINT detected by MASTER\n");
+        else
+            printf("MASTER: SIGNAL: SIGALRM detected by MASTER\n");
+
+        for (i = 0; i < numChildren; i++) {
+            kill(pid[i].actualPid, SIGINT);
+        }
+
+        while (wait(&wait_status) > 0) { ; }
+
+        free(pid);
+        detachAndRemove(sharedMemId, shm);
+        exit(0);
+    }
+}
+
+/*************************************************!
+* @function    getIndex
+* @abstract    get the index from the array of
+*              children.
+* @param       pidIndex index of the pidIndex
+* @returns     actual index from the pid array
+**************************************************/
+int getIndex(int logical) {
+    int i;
+    for(i = 0; i < numChildren; i++)
+        if(pid[i].pidIndex == logical)
+            return i;
+    return -1;
 }
---
+++ christop.2/shared.h
@@ -0,0 +1,20 @@
+#ifndef CHRISTOP_2_SHARED_H
+#define CHRISTOP_2_SHARED_H
+
+#include <ntsid.h>
+
+#define SHARED_MEM_KEY 1775
+
+
+
+typedef struct SharedClock {
+    int seconds;
+    int milliseconds;
+} SharedMemClock;
+
+typedef struct ProcessContainer {
+    int pidIndex;
+    pid_t actualPid;
+} Process;
+
+#endif //CHRISTOP_2_SHARED_H
