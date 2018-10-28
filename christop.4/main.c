b0abd895d45d64b3e44357b089fae4b4f71cd474
Chris Thompson <christhompson@MacBook-Pro-8.local>
2018-10-28 17:15:45 -0500

    Added methods to advance the clock, generate the high and low priority queues, set up the shared memory clock, set up the process control block, and do signal handling. Added alarm for the timer, signal handling, and setting up the message queue. Added Queue, UserProcess, ProcessControlBlock structs.

--- christop.4/main.c
+++ christop.4/main.c
@@ -20,31 +20,46 @@
 #include <sys/msg.h>
 #include "shared.h"
 
-#define DEFAULT_TIMER 20
-#define DEFAULT_FILENAME "log.out"
-#define TOTAL_PROCESSES 100
 
-/* GLOBALS */
-PCB pcb[18];
-int numChildren;
-int sharedMemId;
-int queueId;
-SharedMemClock *shm;
-int indexCounter;
-int numTerminated = 0;
+/* GLOBAL STRUCTS */
+PCB* pcb;
+SharedMemClock* sharedMemClock;
+UserProcess* userProcess;
+
+/* GLOBAL MISC */
 char* fileName;
+char* pcbAddress;
+int indexCounter;
+int numChildren;
+int numTerminated;
+int pcbSharedMemId;
+int queueSharedMemId;
+int sharedMemClockId;
 int timerAmount;
+
 
 /* PROTOTYPES */
 void displayHelp(int);
 int detachAndRemove(int, void*);
 long checkLogFile(FILE*);
 int randomNumberGenerator(int, int);
+void signalHandlerMaster(int);
+void processControlBlocksSetup();
+void sharedMemoryClockSetup();
+void advanceSharedMemoryClock();
 
+/*************************************************!
+* @function    main
+* @abstract    orchestrates the madness
+* @param       argc
+* @param       argv
+* @returns     0... hopefully
+**************************************************/
 int main(int argc, char **argv) {
-    int c;
+    int c, wait_status;
     char* lvalue = NULL;
     char* tvalue = NULL;
+
 
     while ((c = getopt (argc, argv, "h l:t:")) != -1) {
         switch (c) {
@@ -72,25 +87,39 @@ int main(int argc, char **argv) {
     timerAmount = (tvalue == NULL) ? DEFAULT_TIMER : atoi(tvalue);
     (lvalue == NULL) ? strcpy(fileName, DEFAULT_FILENAME) : strcpy(fileName, lvalue);
 
+    /* register signal handler (SIGINT) */
+    if (signal(SIGINT, signalHandlerMaster) == SIG_ERR) {
+        perror("Error: Couldn't catch SIGINT\n");
+        exit(errno);
+    }
+
+    /* register signal handler (SIGALRM) */
+    if (signal(SIGALRM, signalHandlerMaster) == SIG_ERR) {
+        perror("Error: Couldn't catch SIGALRM\n");
+        exit(errno);
+    }
+
+    /* set up the timer */
+    alarm(timerAmount);
+
     /* create message queue */
-    queueId = msgget(QUEUE_KEY, IPC_CREAT | 0600);
+    queueSharedMemId = msgget(QUEUE_KEY, IPC_CREAT | 0600);
 
-    /* create shared memory segment */
-    if ((sharedMemId = shmget(SHARED_MEM_KEY, sizeof(SharedMemClock), IPC_CREAT | 0600)) < 0) {
-        perror("[-]ERROR: Failed to create shared memory segment.");
-        exit(errno);
-    }
+    /* set up the shared memory clock */
+    sharedMemoryClockSetup();
 
-    /* create shared memory segment */
-    if ((sharedMemId = shmget(SHARED_MEM_KEY, sizeof(pcb), IPC_CREAT | 0600)) < 0) {
-        perror("[-]ERROR: Failed to create shared memory segment.");
-        exit(errno);
-    }
+    /* set up the process control blocks */
+    processControlBlocksSetup();
+
+    /*  */
+
+    /* wait for any remaining child processes to finish */
+    while (wait(&wait_status) > 0) { ; }
 
     /* detach and remove the message queue, shared memory, and any allocated memory */
     free(pcb);
-    detachAndRemove(sharedMemId, shm);
-    msgctl(queueId, IPC_RMID, NULL);
+    detachAndRemove(sharedMemClockId, sharedMemClock);
+    msgctl(queueSharedMemId, IPC_RMID, NULL);
     return 0;
 }
 
@@ -124,7 +153,7 @@ long checkLogFile(FILE* fp) {
     long numLines = 0;
     char c;
 
-    fp = fopen(fileName, "r");
+//    fp = fopen(fileName, "r");
     for (c = getc(fp); c != EOF; c = getc(fp))
         if (c == '\n')
             numLines += 1;
@@ -159,4 +188,116 @@ void displayHelp(int flag) {
         printf("\t-t z        : Set time (in seconds) before master terminates. Defaults to '%d'.\n", DEFAULT_TIMER);
         exit(0);
     }
+}
+
+/*******************************************************!
+* @function    signalHandler
+* @abstract    performs all necessary signal handling
+*
+* @param       pidIndex index of the pidIndex
+* @returns     actual index from the pid array
+*******************************************************/
+void signalHandlerMaster(int signo) {
+    int i, wait_status;
+
+    if (signo == SIGINT || signo == SIGALRM) {
+        if (signo == SIGINT)
+            printf("MASTER: SIGNAL: SIGINT detected by MASTER\n");
+        else
+            printf("MASTER: SIGNAL: SIGALRM detected by MASTER\n");
+
+        for (i = 0; i < numChildren; i++) {
+            kill(pcb[i].actualPid, SIGINT);
+        }
+
+        while (wait(&wait_status) > 0) { ; }
+
+        free(pcb);
+        detachAndRemove(sharedMemClockId, sharedMemClock);
+        detachAndRemove(pcbSharedMemId, pcb);
+        msgctl(queueSharedMemId, IPC_RMID, NULL);
+        exit(0);
+    }
+}
+
+/*******************************************************!
+* @function    processControlBlocksSetup
+* @abstract    sets up the process control blocks
+*******************************************************/
+void processControlBlocksSetup() {
+    /* Allocate memory for user processes, but will not be in shared memory. Seems easier to manage this way */
+    userProcess = (struct UserProcess*) malloc(sizeof(struct UserProcess) * 18);
+
+    /* allocate some memory for the array of processes */
+    pcb = (struct ProcessControlBlock*) malloc(sizeof(struct ProcessControlBlock) * 18);
+
+    /* create Process Control Block shared memory segment */
+    if ((pcbSharedMemId = shmget(SHARED_MEM_PCB_KEY, sizeof(pcb), IPC_CREAT | 0600)) < 0) {
+        perror("[-]ERROR: Failed to create shared memory segment.");
+        exit(errno);
+    }
+
+    /* attach the shared memory process control blocks */
+    pcbAddress = shmat(pcbSharedMemId, NULL, 0);
+
+    //initialize pcb with slaveID = -1;
+    pcb = (PCB*) ((void*)pcbAddress+sizeof(int));
+
+    int i;
+    for(i = 0; i < 18; i++) {
+
+    }
+}
+
+/*******************************************************!
+* @function    sharedMemoryClockSetup
+* @abstract    sets up the shared memory clock
+*******************************************************/
+void sharedMemoryClockSetup() {
+    /* create Shared Memory Clock shared memory segment */
+    if ((sharedMemClockId = shmget(SHARED_MEM_CLOCK_KEY, sizeof(SharedMemClock), IPC_CREAT | 0600)) < 0) {
+        perror("[-]ERROR: Failed to create shared memory segment.");
+        exit(errno);
+    }
+
+    /* attach the shared memory clock */
+    sharedMemClock = shmat(sharedMemClockId, NULL, 0);
+
+    /* initialize the shared memory clock */
+    sharedMemClock->seconds = 0;
+    sharedMemClock->nanoSeconds = 0;
+}
+
+/*******************************************************!
+* @function    advanceClock
+* @abstract    advances the shared memory clock by 1.xx
+*              where xx is random number in range
+*              [0, 1000]
+*******************************************************/
+void advanceSharedMemoryClock() {
+    unsigned int tempVar = 0;
+    int nanoRandom = randomNumberGenerator(1000, 0);
+    sharedMemClock->nanoSeconds += nanoRandom;
+    sharedMemClock->seconds += 1;
+    if(sharedMemClock->nanoSeconds >= 999999999) {
+        tempVar = sharedMemClock->nanoSeconds - 999999999;
+        sharedMemClock->seconds += 1;
+        sharedMemClock->nanoSeconds = tempVar;
+    }
+}
+
+/*******************************************************!
+* @function    generateQueue
+* @abstract    generates the Queue
+* @param       capacity
+* @returns     queue
+*******************************************************/
+struct Queue* generateQueue(unsigned int capacity) {
+    /* allocate space for the Queue and initialize the queue */
+    struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue));
+    queue->queueCapacity = capacity;
+    queue->front = queue->size = 0;
+    queue->rear = capacity - 1;
+    queue->array = (int*)malloc(queue->queueCapacity * sizeof(int));
+    return queue;
 }
--- christop.4/shared.h
+++ christop.4/shared.h
@@ -1,15 +1,20 @@
 #pragma once
 #ifndef O2_THOMPSON_3_SHARED_H
 #define O2_THOMPSON_3_SHARED_H
-#define SHARED_MEM_KEY 1775
+#define SHARED_MEM_CLOCK_KEY 1775
+#define SHARED_MEM_PCB_KEY 8541
 #define QUEUE_KEY 4604
 #define MASTER_ID 369
+
+#define DEFAULT_TIMER 20
+#define DEFAULT_FILENAME "log.out"
+#define TIME_QUANTUM
 
 #include <stdlib.h>
 
 typedef struct SharedClock {
-    int seconds;
-    int nanoSeconds;
+    unsigned int seconds;
+    unsigned int nanoSeconds;
 } SharedMemClock;
 
 typedef struct Message {
@@ -18,13 +23,29 @@ typedef struct Message {
     int doneFlag;
     int seconds;
     int nanoSeconds;
-} Envelope;
+} Message;
 
 typedef struct ProcessControlBlock {
     int pidIndex;
     pid_t actualPid;
-    int seconds;
-    int nanoSeconds;
 } PCB;
+
+typedef struct UserProcess {
+    int index;
+    pid_t actualPid;
+    int priority;
+    int duration;
+    int progress;
+    int burstTime;
+
+} UserProcess;
+
+typedef struct Queue {
+    int* array;
+    int front;
+    int rear;
+    int size;
+    unsigned int queueCapacity;
+} Queue;
 
 #endif //O2_THOMPSON_4_SHARED_H
