265c99d9bc95b96c568a37430db3d0fa89da9f80
Chris Thompson <christhompson@MacBook-Pro-8.local>
2018-11-26 18:45:11 -0600

    initial commit

---
+++ christop.6/cmake-build-debug/CMakeFiles/christop_6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/christop_6.dir/main.c.o"
+  "christop_6.pdb"
+  "christop_6"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/christop_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
